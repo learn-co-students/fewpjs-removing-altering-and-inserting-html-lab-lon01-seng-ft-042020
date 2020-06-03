0c8ada5e98e51eb4ffd37ebe1a70ae4a4fb3cd3a
jsymondst <jammiesymondstayler@gmail.com>
2020-06-03 12:30:55 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+
+document.body.appendChild(newHeader);
+
+newHeader.id ="victory";
+newHeader.innerHTML = "Jamie is the champion";
