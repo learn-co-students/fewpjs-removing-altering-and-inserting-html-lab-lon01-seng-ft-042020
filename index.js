27c656bf18b15415db958cd66c04aa04da19b3ef
ewelinaszoda <ewelinaszoda@gmail.com>
2020-06-21 22:13:57 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader)
+
+newHeader.textContent = 'is the champion';
+
